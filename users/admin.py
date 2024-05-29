a685ed342d418c99aaacfcf6734ce17fc6ec41c3
aLEKS-e3 <chebukin404@gmail.com>
2024-05-29 06:55:31 +0300

    Set up an avatar upload functionality

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    search_fields = ("username",)
+    list_display = UserAdmin.list_display + ("avatar",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("avatar",)}),)
+    )
--- users/models.py
+++ users/models.py
@@ -5,14 +5,13 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
-from django.utils.text import slugify
 
 
 def avatar_image_path(instance: User, filename: str) -> str:
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.username)}-{uuid.uuid4()}{extension}"
+    filename = f"{instance.username}{extension}"
 
-    return os.path.join("uploads/crew/", filename)
+    return os.path.join("uploads/avatars/", filename)
 
 
 class User(AbstractUser):
