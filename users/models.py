f03278458c68b22ab4542b7f230ecee25805310c
aLEKS-e3 <chebukin404@gmail.com>
2024-05-29 06:13:24 +0300

    Initialize a user model with avatar

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from __future__ import annotations
+import os
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
+
+def avatar_image_path(instance: User, filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.username)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/crew/", filename)
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(null=True, upload_to=avatar_image_path)
+
+    class Meta:
+        ordering = ("username",)
+    
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"pk": self.pk})
+    
+    def __str__(self) -> str:
+        return self.username
