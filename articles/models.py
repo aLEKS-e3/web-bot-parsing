f0fb02ad80c4540adb48f5c640998d2798916606
aLEKS-e3 <chebukin404@gmail.com>
2024-06-01 19:30:33 +0300

    Initialize Article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publication_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
