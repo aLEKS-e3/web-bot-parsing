fa6c34f42a6a2250aa4ae0d904f7ca308fadab25
aLEKS-e3 <chebukin404@gmail.com>
2024-06-01 16:49:47 +0300

    Add urls and settings to the main project

--- magazine/urls.py
+++ magazine/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("users.urls")),
     path("", include("articles.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
