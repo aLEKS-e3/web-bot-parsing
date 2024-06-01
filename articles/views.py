b03fa52adb58103b52cc37831d8e9bb23940b838
aLEKS-e3 <chebukin404@gmail.com>
2024-06-01 15:47:03 +0300

    Initialize a view to handle home pages based on user auth

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        return render(request, "articles/home.html")
+    return render(request, "articles/index.html")
